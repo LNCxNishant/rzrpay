5de81e6d9139743e42de1cb097082c9dcb4ed318
LNCxNishant <nishantpatil8433@gmail.com>
2023-12-19 17:09:19 +0530

    rzrpay

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env:{
+    //RazorPay keys
+    RAZORPAY_KEY:'rzp_test_tA3L5vLUV9Ljsl',
+    RAZORPAY_SECRET:'ggjX8e4TGrIkjxBsXGXqT1mK',
+},
 }
 
 module.exports = nextConfig
